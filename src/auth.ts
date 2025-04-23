6869e9b76d53eaaae74c42e304b9938aebc0ae97
Byoungcheon Lee <lbcsultan@gmail.com>
2025-04-23 17:10:58 +0900

    4th-mongodb

--- src/auth.ts
+++ src/auth.ts
@@ -12,4 +12,28 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: '/signIn',
   },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === 'google' || account?.provider === 'github') {
+        try {
+          const apiUrl = process.env.NEXT_PUBLIC_API_URL
+
+          const res = await fetch(`${apiUrl}/api/user-auth`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ user, account }),
+          })
+          if (res.ok) {
+            return true
+          }
+        } catch (error) {
+          console.log(error)
+          return false
+        }
+      }
+      return true
+    },
+  },
 })
